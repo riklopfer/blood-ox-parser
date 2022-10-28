157fc0725e05a09f67add16fc9edb934f790f5dd
Russell Klopfer <russell@kloper.us>
2022-10-28 18:16:26 -0400

    bin width

--- elevation-histogram.py
+++ elevation-histogram.py
@@ -26,8 +26,9 @@ def to_elevation_meters(kpa: float) -> float:
     return (1 - pow(p / 101325, 1 / 5.25588)) / 2.25577e-5
 
 
-def get_hist_bin(elevation: float) -> float:
-    return (elevation // 10) * 10
+def get_hist_bin(elevation: float, bin_width: int) -> float:
+    # return round(elevation / bin_width) * bin_width
+    return (elevation // bin_width) * bin_width
 
 
 def hist_to_str(histogram: Dict) -> str:
@@ -45,15 +46,17 @@ def hist_to_str(histogram: Dict) -> str:
 
 
 def main(args: argparse.Namespace):
+    in_path = args.bloodox
+    bin_width = args.bin_width
     histogram = defaultdict(list)
-    with open(args.bloodox, "r") as ifp:
+    with open(in_path, "r") as ifp:
         reader = csv.reader(ifp)
         # skip header
         _ = next(reader)
         for tstmp, bloodox, kpa in reader:
             bloodox, kpa = map(float, (bloodox, kpa))
             elevation = to_elevation_meters(kpa)
-            histogram[get_hist_bin(elevation)].append(bloodox)
+            histogram[get_hist_bin(elevation, bin_width)].append(bloodox)
 
     hist_str = hist_to_str(histogram)
     print(hist_str)
@@ -63,6 +66,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "--bloodox", help="Extracted blood oxygen CSV", type=str, default="bloodox.csv"
+    )
+
+    parser.add_argument(
+        "--bin-width", help="Bin width for histogram", type=int, default=100, choices=[1, 10, 100, 1000]
     )
 
     main(parser.parse_args())
